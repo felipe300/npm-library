cc92165c997d9b94d35323bce05fb7e934f44368
Felipe300 <caracoloro@gmail.com>
2023-02-21 23:12:59 -0300

    feat: CI

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest"
+
+describe("Whatever", () => {
+  it("It should pass CI", () => {
+    expect(1).toBe(1)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export const hello = "Hi"
+export const hello = () => {
+  console.log("Hi")
+}
